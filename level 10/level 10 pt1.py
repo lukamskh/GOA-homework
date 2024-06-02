c33eff8dd11858e1cd5a8de350e60c0ea84abedc
lukamskh <mskhaladze.luka7@students.gov.ge>
2024-06-02 18:24:07 +0400

    commit

--- level 10/level 10 pt1.py
+++ level 10/level 10 pt1.py
@@ -9,8 +9,8 @@ for number in range (10):
     print(number + 1)
 print("finish")
 print("start")
-for number in range (49):
+for number in range (50):
+    print(num)
     num1 = num + 1
     num = num1 + 1
-    print(num)
 print("finish")
