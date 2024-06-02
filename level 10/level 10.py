49134b8aa9f330e735f6932b288ae71b84080f8c
lukamskh <mskhaladze.luka7@students.gov.ge>
2024-06-02 18:19:47 +0400

    commit

---
+++ level 10/level 10.py
@@ -0,0 +1,16 @@
+
+num = 2
+print("start")
+for number in range (20):
+    print(number + 1)
+print("finish")
+print("start")
+for number in range (10):
+    print(number + 1)
+print("finish")
+print("start")
+for number in range (49):
+    num1 = num + 1
+    num = num1 + 1
+    print(num)
+print("finish")
