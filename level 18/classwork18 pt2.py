b24056cb77e3f4d97690a789f8cdf2a54c025ba9
lukamskh <mskhaladze.luka7@students.gov.ge>
2024-06-22 16:28:29 +0400

    commit

--- level 18/classwork18 pt2.py
+++ level 18/classwork18 pt2.py
@@ -1,6 +1,9 @@
-admin = "luka"
-person = str(input("enter your name: "))
-if person == admin:
-    print("hello admin")
+num = int(input("enter amount of seconds: "))
+num2 = int(input("enter amount: "))
+for i in range (num):
+    i = i + 1
+    print(i)
+if i <= num2:
+    print("your number is below " + str(num2))
 else:
-    print("hello user")
+    print("your number is above " + str(num2))
